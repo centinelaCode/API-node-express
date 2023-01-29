ca369e179fde0366af325262a328996bc610216b
rvaldez20 <rvaldez20@gmail.com>
2023-01-29 15:46:25 -0600

    [ADD] Se crean más routes para la aplicación

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-   res.send('Route: Home /')
+   res.send('Route: Home -> /')
+})
+app.get('/nueva-ruta', (req, res) => {
+   res.send('Route: NuevaRuta -> /nueva-ruta')
+})
+app.get('/products', (req, res) => {
+   res.json(
+      {
+         name: 'Product 1',
+         price: 1000
+      }
+   )
 })
 
 app.listen(PORT, () => {
