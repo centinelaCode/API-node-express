2c13674444711a0372cc8be315f19b5083cbb17e
rvaldez20 <rvaldez20@gmail.com>
2023-01-29 15:23:37 -0600

    [ADD] Configuración de un servidor con express basico

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('My App')
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+   res.send('Route: Home /')
+})
+
+app.listen(PORT, () => {
+   console.log('Server run on Port ', PORT)
+})
