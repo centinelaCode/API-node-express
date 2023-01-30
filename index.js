b1dffe58b5e7a1ea8105487faf9bb06dc13c0d1b
rvaldez20 <rvaldez20@gmail.com>
2023-01-29 21:28:31 -0600

    [ADD] Se agregan routes de products | categories | users
    => Para cada uno las rotes getAll y getOne

--- index.js
+++ index.js
@@ -2,20 +2,109 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+
+// Route Home
 app.get('/', (req, res) => {
-   res.send('Route: Home -> /')
+   res.send('HOME')
 })
-app.get('/nueva-ruta', (req, res) => {
-   res.send('Route: NuevaRuta -> /nueva-ruta')
-})
+
+
+// Route for all products
 app.get('/products', (req, res) => {
    res.json(
-      {
-         name: 'Product 1',
-         price: 1000
-      }
+      [
+         {
+            id: 1,
+            name: 'Product 1',
+            price: 1000
+         },
+         {
+            id: 2,
+            name: 'Product 2',
+            price: 2002
+         },
+      ]
    )
 })
+
+// Route for one product
+app.get('/products/:idProduct', (req, res) => {
+   const { idProduct } = req.params;
+
+   res.json({
+      idProduct,
+      name: 'Product 1',
+      price: 1000
+   })
+})
+
+// Route for all Categories
+app.get('/categories', (req, res) => {
+   res.json(
+      [
+         {
+            id: 1,
+            name: 'Categorie 1',
+         },
+         {
+            id: 2,
+            name: 'Categorie 2',
+         },
+      ]
+   )
+})
+
+// Route for one Category
+app.get('/categories/:idCategory', (req, res) => {
+   const { idCategory } = req.params;
+   res.json(
+         {
+            idCategory,
+            name: 'Categorie 1',
+         }
+   )
+})
+
+// route for categories-products
+app.get('/categories/:idCategory/products/:idProduct', (req, res) => {
+   const { idCategory, idProduct } =req.params;
+   res.json({
+      idCategory,
+      idProduct,
+   })
+})
+
+
+// Route for all Users
+app.get('/users', (req, res) => {
+   res.json(
+      [
+         {
+            id: 1,
+            name: 'User 1',
+            email: 'user1@correo.com'
+         },
+         {
+            id: 2,
+            name: 'User 2',
+            email: 'user2@correo.com'
+         },
+      ]
+   )
+})
+
+// Route for one User
+app.get('/users/:idUser', (req, res) => {
+   const {idUser} = req.params;
+   res.json(
+         {
+            idUser,
+            name: 'user#@correo.com',
+         }
+   )
+})
+
+
 
 app.listen(PORT, () => {
    console.log('Server run on Port ', PORT)
